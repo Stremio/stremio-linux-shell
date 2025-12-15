84dbd8022e5c6dedecb0efa92053ec4646453a93
Tim <tymmesyde@gmail.com>
2025-12-15 08:28:12 +0100

    chore: typo in startup url

--- src/config.rs
+++ src/config.rs
@@ -4,4 +4,4 @@ pub const GETTEXT_DOMAIN: &str = "stremio";
 pub const GETTEXT_DIR_DEV: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/po");
 pub const GETTEXT_DIR_FLATPAK: &str = "/app/share/locale";
 
-pub const STARTUP_URL: &str = "https:/web.stremio.com";
+pub const STARTUP_URL: &str = "https://web.stremio.com";
