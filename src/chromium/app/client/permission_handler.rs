5159390b49950e650e5d213b006a67f8c04e8b6d
Tim <tymmesyde@gmail.com>
2026-02-14 10:46:38 +0100

    chore(chromium): clippy

--- src/chromium/app/client/permission_handler.rs
+++ src/chromium/app/client/permission_handler.rs
@@ -17,13 +17,11 @@ wrap_permission_handler! {
             requested_permissions: u32,
             callback: Option<&mut PermissionPromptCallback>,
         ) -> i32 {
-            if requested_permissions == CEF_PERMISSION_TYPE_LOCAL_NETWORK_ACCESS as u32
-                || requested_permissions == CEF_PERMISSION_TYPE_LOOPBACK_NETWORK as u32
-            {
-                if let Some(callback) = callback {
-                    callback.cont(CEF_PERMISSION_RESULT_ACCEPT.into());
-                    return true.into();
-                }
+            if (requested_permissions == CEF_PERMISSION_TYPE_LOCAL_NETWORK_ACCESS as u32
+                || requested_permissions == CEF_PERMISSION_TYPE_LOOPBACK_NETWORK as u32)
+            && let Some(callback) = callback {
+                callback.cont(CEF_PERMISSION_RESULT_ACCEPT.into());
+                return true.into();
             }
 
             Default::default()
