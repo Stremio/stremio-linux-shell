771cbc39f43a2f8d81d60f9f1d01acdb54ba1f29
Tim <tymmesyde@gmail.com>
2026-05-02 06:20:03 +0200

    refactor(ipc): prevent panic when parsing requests

--- src/app/ipc/request.rs
+++ src/app/ipc/request.rs
@@ -23,7 +23,8 @@ impl TryFrom<IpcMessageRequest> for IpcEvent {
             3 => Ok(IpcEvent::Init),
             6 => match value.args {
                 Some(args) => {
-                    let args: Vec<Value> = serde_json::from_value(args).expect("Invalid arguments");
+                    let args: Vec<Value> =
+                        serde_json::from_value(args).map_err(|_| "Invalid arguments")?;
                     let name = args.first().and_then(Value::as_str).ok_or("Invalid name")?;
                     let data = args.get(1).cloned();
 
@@ -33,13 +34,13 @@ impl TryFrom<IpcMessageRequest> for IpcEvent {
                             "win-set-visibility" => {
                                 let data: IpcMessageRequestWinSetVisilibty =
                                     serde_json::from_value(data)
-                                        .expect("Invalid win-set-visibility object");
+                                        .map_err(|_| "Invalid win-set-visibility object")?;
 
                                 Ok(IpcEvent::Fullscreen(data.fullscreen))
                             }
                             "mpv-command" => {
                                 let data: Vec<String> = serde_json::from_value(data)
-                                    .expect("Invalid mpv-command arguments");
+                                    .map_err(|_| "Invalid mpv-command arguments")?;
                                 let name = data[0].clone();
 
                                 let mut args = vec![];
@@ -50,21 +51,21 @@ impl TryFrom<IpcMessageRequest> for IpcEvent {
                                 Ok(IpcEvent::Mpv(IpcEventMpv::Command((name, args))))
                             }
                             "mpv-observe-prop" => {
-                                let name = data.as_str().expect("Invalid mpv-observe-prop name");
+                                let name = data.as_str().ok_or("Invalid mpv-observe-prop name")?;
                                 Ok(IpcEvent::Mpv(IpcEventMpv::Observe(name.to_owned())))
                             }
                             "mpv-set-prop" => {
                                 let key_value: Vec<Value> = serde_json::from_value(data)
-                                    .expect("Invalid mpv-set-prop arguments");
+                                    .map_err(|_| "Invalid mpv-set-prop arguments")?;
 
                                 let name = key_value[0]
                                     .as_str()
-                                    .expect("Invalid mpv-set-prop name")
+                                    .ok_or("Invalid mpv-set-prop name")?
                                     .to_owned();
 
                                 let value = key_value
                                     .get(1)
-                                    .expect("Invalid mpv-set-prop value")
+                                    .ok_or("Invalid mpv-set-prop value")?
                                     .to_owned();
 
                                 Ok(IpcEvent::Mpv(IpcEventMpv::Set((name, value))))
