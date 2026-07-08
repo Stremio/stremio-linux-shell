e6fb2166d458d9cb962c2af1cf61996f7326925a
Tim <tymmesyde@gmail.com>
2026-07-08 21:36:29 +0200

    build: add deb package

--- data/stremio.sh
+++ data/stremio.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+export LC_NUMERIC=C
+export ANV_DEBUG=video-decode,video-encode
+export SERVER_PATH=/usr/libexec/stremio/server.js
+
 # Use GSK OpenGL renderer for Nvidia cards
 if ls /dev/nvidia0 &>/dev/null 2>&1; then
     export GSK_RENDERER=opengl
 fi
 
-exec /app/libexec/stremio/stremio "$@"
+exec /usr/libexec/stremio/stremio "$@"
--- src/config.rs
+++ src/config.rs
@@ -2,7 +2,7 @@ pub const DATA_DIR: &str = "stremio";
 
 pub const GETTEXT_DOMAIN: &str = "stremio";
 pub const GETTEXT_DIR_DEV: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/po");
-pub const GETTEXT_DIR_FLATPAK: &str = "/app/share/locale";
+pub const GETTEXT_DIR_FLATPAK: &str = "/usr/share/locale";
 
 pub const STARTUP_URL: &str = "http://127.0.0.1:11470/proxy/d=https%3A%2F%2Fweb.stremio.com/";
 pub const IPC_KEY: &str = "LINUX";
